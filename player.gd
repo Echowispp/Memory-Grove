7c21160b5f490a45705c92cf26815e218b464cc4
Echowispp <kauri.sykari190@gmail.com>
2025-10-23 22:12:26 +0300

    Animations and movement working
    also added a rest animation after staying still for 10 seconds

--- player.gd
+++ player.gd
@@ -1,11 +1,55 @@
 extends Area2D
 
+@export var speed = 100
+var screen_size
+var idle_time = 0.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var animated_sprite = $AnimatedSprite2D
+## Might not need the collision shape to be shortcut,
+## but it's good to have easily available
+#@onready var collision_shape = $CollisionShape2D
+
+func _ready():
+	screen_size = get_viewport_rect().size
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta: float):
+	var velocity = Vector2.ZERO
+	if (Input.is_action_pressed ("move_right") ):
+		velocity.x += 1
+	if (Input.is_action_pressed ("move_left") ):
+		velocity.x -= 1
+	if (Input.is_action_pressed ("move_up") ):
+		velocity.y -= 1
+	if (Input.is_action_pressed ("move_down") ):
+		velocity.y += 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		position += velocity * delta
+		idle_time = 0.0
+
+		var movement_angle_rad = velocity.angle()
+		var movement_angle = movement_angle_rad * 180 / PI
+		if movement_angle >= -22.5 and movement_angle < 22.5:
+			animated_sprite.play("walk_right")
+		elif movement_angle >= 22.5 and movement_angle < 67.5:
+			animated_sprite.play("walk_SE")
+		elif movement_angle >= 67.5 and movement_angle < 112.5:
+			animated_sprite.play("walk_down")
+		elif movement_angle >= 112.5 and movement_angle < 157.5:
+			animated_sprite.play("walk_SW")
+		elif movement_angle >= 157.5 or movement_angle < -157.5:
+			animated_sprite.play("walk_left")
+		elif movement_angle >= -157.5 and movement_angle < -112.5:
+			animated_sprite.play("walk_NW")
+		elif movement_angle >= -112.5 and movement_angle < -67.5:
+			animated_sprite.play("walk_up")
+		elif movement_angle >= -67.5 and movement_angle < -22.5:
+			animated_sprite.play("walk_NE")
+	elif idle_time >= 10:
+		animated_sprite.play("rest")
+	elif idle_time < 10:
+		animated_sprite.play("idle")
+		idle_time += delta
+	
